c5d5a5ef52a9178c7eb19c6628756d9d5a47fb5a
amanda-bann <amanda_klap@protonmail.com>
2024-10-18 19:55:12 -0600

    Create a function to remove duplicates

--- js/script.js
+++ js/script.js
@@ -18,3 +18,22 @@ var groceries = [
   "olive oil",
   "mustard"
 ];
+
+// Create a function to remove duplicates from the groceries array //
+//Log out the length of the groceries variable to see how many elements are in the array
+
+console.log(groceries.length);
+
+// Create a new variable called deleteDuplicates with list as a parameter
+var deleteDuplicates = function(list) {
+  var cleanList = [];
+
+  for (var item of list) {
+    if (!cleanList.includes(item)) {
+      cleanList.push(item);
+    }
+  }
+
+  return cleanList;
+};
+
